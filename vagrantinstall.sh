e9b89002c14288e7e882498d3c3f697cb1411364
mdnfiras <mdnfiras@yahoo.com>
2020-07-27 14:35:28 +0100

    modified vagrant install script

--- vagrantinstall.sh
+++ vagrantinstall.sh
@@ -1,1 +1,2 @@
-apt update & apt -y install qemu-kvm libvirt-bin ebtables dnsmasq-base libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev vagrant
+apt update
+apt -y install qemu-kvm libvirt-bin ebtables dnsmasq-base libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev vagrant
