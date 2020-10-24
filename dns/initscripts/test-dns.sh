910be2a060e3d5ddf179ee978505804821035744
mdnfiras <mdnfiras@yahoo.com>
2020-10-24 01:52:35 +0100

    modified DNS tests

--- dns/initscripts/test-dns.sh
+++ dns/initscripts/test-dns.sh
@@ -5,8 +5,8 @@ named-checkzone 5.168.192.in-addr.arpa. /etc/bind/db.192
 named-checkconf  /etc/bind/named.conf.local
 named-checkconf  /etc/bind/named.conf
 
-nslookup ns.[[DOMAIN]] 192.168.5.3
-nslookup nfs.[[DOMAIN]] 192.168.5.3
-nslookup vpn.[[DOMAIN]] 192.168.5.3
-nslookup devops.[[DOMAIN]] 192.168.5.3
-nslookup jenkins.devops.[[DOMAIN]] 192.168.5.3
+nslookup ns.[[DOMAIN]]
+nslookup nfs.[[DOMAIN]]
+nslookup vpn.[[DOMAIN]]
+nslookup devops.[[DOMAIN]]
+nslookup jenkins.devops.[[DOMAIN]]
