b2235a4c5fafe0ce65193e0095212fb1d4c345f6
mdnfiras <mdnfiras@yahoo.com>
2020-11-06 00:19:41 +0100

    vagrant download

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,7 @@
 apt update
-apt -y install qemu-kvm libvirt-bin ebtables dnsmasq-base libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev vagrant software-properties-common
+apt -y install qemu-kvm libvirt-bin ebtables dnsmasq-base libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev software-properties-common
+wget https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_x86_64.deb
+dpkg -i vagrant_2.2.10_x86_64.deb
 vagrant plugin install vagrant-libvirt
 apt-add-repository --yes --update ppa:ansible/ansible
 apt -y install ansible
