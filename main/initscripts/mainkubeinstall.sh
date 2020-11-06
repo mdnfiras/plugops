98f1a1f528056692e1dd1ccc3b67779fdeff2156
mdnfiras <mdnfiras@yahoo.com>
2020-11-06 22:28:57 +0100

    wait for jenkins ui

--- main/initscripts/mainkubeinstall.sh
+++ main/initscripts/mainkubeinstall.sh
@@ -112,6 +112,9 @@ kubectl apply -f jenkins/jenkins-ingress.yaml
 echo "=======> [jenkins] waiting for jenkins' pod to run :"
 while [[ -z "`kubectl get pods | grep jenkins | grep Running`" ]]; do sleep 5; done
 
+echo "=======> [jenkins] waiting for jenkins' web interface to run :"
+while [[ -z "`curl jenkins.devops.[[DOMAIN]] | grep Authentication | grep required`" ]]; do sleep 5; done
+
 echo "=======> [jenkins] printing jenkins' admin password :"
 kubectl cp $( kubectl get pods | grep jenkins | tr -s ' ' | cut -d ' ' -f1 ):/var/jenkins_home/secrets/initialAdminPassword ./jenkinsInitialAdminPassword
 cat jenkinsInitialAdminPassword
